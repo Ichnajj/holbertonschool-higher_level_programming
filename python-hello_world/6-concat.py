0074206520001f91d42ecb820d364f68bf31bdfc
ichnajj <10025@holbertonstudents.com>
2025-01-16 15:10:06 +0100

    concat  5lines

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-print(f"Welcome to {str1} {str2}!")
+# Concatenate str1 and str2 in the print statement
+print("Welcome to {} {}".format(str1, str2) + "!")
