5d5070103426f7c544e8e7109dc7867feb54a10a
ichnajj <10025@holbertonstudents.com>
2025-01-16 15:18:05 +0100

    concat edges coorection

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-str1 = "object"
-str2 = "-oriented"
-str3 = "programming"
-str4 = "with"
-str5 = "Python"
-print(str1 + str2 + " " + str3 + " " + str4 + " " + str5)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(str)
